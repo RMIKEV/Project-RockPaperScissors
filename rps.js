5871a053105858524bd91d591126a080556d8ea4
RMIKEV <90235398+RMIKEV@users.noreply.github.com>
2024-04-08 15:04:14 +0530

    play 5 rounds deterime who wins b/w player and computer.

--- rps.js
+++ rps.js
@@ -9,6 +9,75 @@ function getComputerChoice(){
 // get user choice
 function getUserChoice(){
     let inp = prompt("choose anything from rock paper and scissors");
+    inp = inp.toLowerCase();
+    
     return inp;
 }
+
+// play round
+function play(playerSelection, computerselection, count){
+    if(playerSelection === computerselection)
+        console.log("tie");
+    
+    else if( playerSelection === "rock" && computerselection == "paper")
+    {
+        console.log("you lose! paper beats rock");
+    }
+        
+    
+    else if( playerSelection === "rock" && computerselection == "scissor"){
+        console.log("you win! rock beats scissor");
+        return count+1;
+    }
+    
+    else if( playerSelection === "paper" && computerselection == "rock"){
+        
+        console.log("you win! paper beats rock");
+        return count+1;
+    }
+    
+    else if( playerSelection === "paper" && computerselection == "scissor"){
+        console.log("you lose! scissor beats paper");
+    }
+
+    else if( playerSelection === "scissor" && computerselection == "paper"){
+        
+        console.log("you win! scissor beats paper");
+        return count+1;
+    }
+    
+    else if( playerSelection === "scissor" && computerselection == "rock"){
+        console.log("you lose! rock beats scissor");
+    }
+
+
+
+}
+
+function playGame(){
+    let count = 0;
+    for(let i =0; i<5; ++i){
+        let userInput = getUserChoice();
+        let compInput = getComputerChoice();
+        count = play(userInput, compInput, count); 
+        console.log(count);
+    }
+
+    if(count == 0){
+        console.log("draw");
+    }
+        
+    else if(count>2){
+        console.log("player wins");
+    }
+        
+    else{
+        console.log("computer wins");
+    }
+        
+}
+
+playGame();
+
+
 
