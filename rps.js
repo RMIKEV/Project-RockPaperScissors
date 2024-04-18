ddd6dbc6ccf3a3dfe03fad9dc23b030093b7414c
RMIKEV <90235398+RMIKEV@users.noreply.github.com>
2024-04-19 02:37:24 +0530

    add event lister for button add css for js file

--- rps.js
+++ rps.js
@@ -1,84 +1,90 @@
-let option =["rock", "paper", "scissor"]
-let count = 0;
-// to generate random choice for computer
-function getComputerChoice(){
-    let computerChoice = option[Math.floor(Math.random() * option.length)];
-    return computerChoice;
+let option = ["rock", "paper", "scissor"]
+
+function getComputerChoice() {
+    // randomly choose from the given list
+    return option[Math.floor(Math.random() * option.length)];
+
 }
 
-// get user choice
-function getUserChoice(){
+
+function getUserChoice() {
     let inp = prompt("choose anything from rock paper and scissors");
     inp = inp.toLowerCase();
-    
+
     return inp;
 }
 
-// play round
-function play(playerSelection, computerselection){
-    if(playerSelection === computerselection)
+let playerCount=0;
+let computerCount =0;
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection)
         console.log("tie");
-    
-    else if( playerSelection === "rock" && computerselection == "paper")
-    {
+
+    else if (playerSelection === "rock" && computerSelection == "paper") {
+        computerCount +=1;
         console.log("you lose! paper beats rock");
     }
-        
-    
-    else if( playerSelection === "rock" && computerselection == "scissor"){
-        count+=1;
+
+
+    else if (playerSelection === "rock" && computerSelection == "scissor") {
+        playerCount += 1;
         console.log("you win! rock beats scissor");
-        
+
     }
-    
-    else if( playerSelection === "paper" && computerselection == "rock"){
-        count+=1;
+
+    else if (playerSelection === "paper" && computerSelection == "rock") {
+        playerCount += 1;
         console.log("you win! paper beats rock");
-        
+
     }
-    
-    else if( playerSelection === "paper" && computerselection == "scissor"){
+
+    else if (playerSelection === "paper" && computerSelection == "scissor") {
+        computerCount +=1;
         console.log("you lose! scissor beats paper");
     }
 
-    else if( playerSelection === "scissor" && computerselection == "paper"){
-        count+=1;
+    else if (playerSelection === "scissor" && computerSelection == "paper") {
+        playerCount += 1;
         console.log("you win! scissor beats paper");
-        
+
     }
-    
-    else if( playerSelection === "scissor" && computerselection == "rock"){
+
+    else if (playerSelection === "scissor" && computerSelection == "rock") {
+        computerCount +=1;
         console.log("you lose! rock beats scissor");
     }
 
-
-
 }
 
-function playGame(){
-    
-    for(let i =0; i<5; ++i){
-        let userInput = getUserChoice();
-        let compInput = getComputerChoice();
-        play(userInput, compInput); 
-        console.log(count);
+
+
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', (event)=>{
+    let target = event.target;
+    let computerSelection = getComputerChoice();
+    switch(target.id){
+        case 'rock': {
+            
+            playRound("rock", computerSelection);
+            break;
+        }
+
+        case 'paper': {
+            
+            playRound("paper", computerSelection);
+            break;
+        }
+
+        case 'scissor': {
+            
+            playRound("scissor", computerSelection);
+            break;
+        }
     }
 
-    if(count == 0){
-        console.log("draw");
-    }
-        
-    else if(count>2){
-        console.log("player wins");
-    }
-        
-    else{
-        console.log("computer wins");
-    }
-        
-}
-
-playGame();
+});
 
 
 
